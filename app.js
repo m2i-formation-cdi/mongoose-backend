1b1b481405f4e79dc9c288d74457ea2e2d0dbe6f
Nazim Hickmet <toto@mail.com>
2019-04-25 12:43:15 +0200

    Inscription d'un utilisateur

--- app.js
+++ app.js
@@ -1,45 +1,60 @@
-const app = require('express')();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const app = require("express")();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require ('bcrypt');
 
-const RestaurantModel = require('./models/restaurants.model');
+const RestaurantModel = require("./models/restaurants.model");
+const UserModel = require("./models/user.model");
 
-const todoRoutes = require('./routes/todo.route');
+const todoRoutes = require("./routes/todo.route");
 
 //Middleware pour la gestion des CORS (permission pour le navigateur d'envoyer une requête à un autre domaine)
 app.use(cors());
 
 //Utilisation de bodyParser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Importation des routes
-app.use('/task', todoRoutes);
+app.use("/task", todoRoutes);
 
-app.get('/', (req, res)=>{
-   let resto = new RestaurantModel(
-      {
-         name: 'Tonio\'s deli',
-         cuisine: 'Italian',
-         borough: 'Queens'
-      }
-   );
+app.get("/", (req, res) => {
+  let resto = new RestaurantModel({
+    name: "Tonio's deli",
+    cuisine: "Italian",
+    borough: "Queens"
+  });
 
-   res.json(resto.save());
+  res.json(resto.save());
 });
 
-app.get('/resto/liste', (req, res)=> {
-   RestaurantModel.find({cuisine: 'Italian', borough: 'Bronx'},'name cuisine',(err, data) =>{
-      if(err){
-         console.log(err);
-         res.json({err: true, message: 'a marche pu'});
-      } else {
-         console.log(data);
-         res.json(data);
-      }
-   });
+app.get("/resto/liste", (req, res) => {
+  RestaurantModel.find({ cuisine: "Italian", borough: "Bronx" }, "name cuisine", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.json({ err: true, message: "a marche pu" });
+    } else {
+      console.log(data);
+      res.json(data);
+    }
+  });
 });
 
+app.post("/register", (req, res) => {
+  let user = new UserModel({
+    userName: req.body.userName,
+    login: req.body.login,
+    password: req.body.password
+  });
 
+  user.save(err => {
+    if (err) {
+      res.json({ success: false, error: err });
+    } else {
+      console.log(user);
+      res.json({ success: true, data: user });
+    }
+  });
+});
 
 app.listen(3000);
